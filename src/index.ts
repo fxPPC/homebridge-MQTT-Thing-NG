f9a6902ad38d97be1b2316c8e15f575358f18d18
Alice <68247743+fxPPC@users.noreply.github.com>
2025-04-21 10:31:19 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-import type { API } from 'homebridge';
+import { API } from 'homebridge';
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
+import { MQTTThingPlatform } from './platform';
 
-import { ExampleHomebridgePlatform } from './platform.js';
-import { PLATFORM_NAME } from './settings.js';
-
-/**
- * This method registers the platform with Homebridge
- */
-export default (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+export = (api: API): void => {
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, MQTTThingPlatform);
 };
